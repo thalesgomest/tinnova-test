d76e9a68051de7ac1f5137b327f2305564b6ade0
Thales Gomes Targino <thalestargino@gmail.com>
2025-03-22 10:18:27 -0300

    feat: adds initial configuration for api development

---
+++ vehicles_rest_api/src/test/java/com/tinnova/vehicles_rest_api/VehiclesRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinnova.vehicles_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclesRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
