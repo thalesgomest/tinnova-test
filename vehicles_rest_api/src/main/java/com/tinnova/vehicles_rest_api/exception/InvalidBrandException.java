4033305d3b586232a5914d1ef7fc0a67f35d7af6
Thales Gomes Targino <thalestargino@gmail.com>
2025-03-22 19:52:10 -0300

    fix: deleted InvalidBrandException

--- vehicles_rest_api/src/main/java/com/tinnova/vehicles_rest_api/exception/InvalidBrandException.java
+++
@@ -1,8 +0,0 @@
-package com.tinnova.vehicles_rest_api.exception;
-
-public class InvalidBrandException extends RuntimeException {
-
-  public InvalidBrandException(String brand) {
-    super("Invalid brand: " + brand);
-  }
-}
